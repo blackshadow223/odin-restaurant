f1e67d80748bece413007dd05b2e2c1f7ef6c1cc
Black Shadow <mithabozdar2@gmail.com>
2025-01-10 16:10:19 +0500

    Make WebPack action ready on a basic level

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve("./dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        host: "127.0.0.1",
+        port: "5500",
+        watchFiles: ["./src/template.html"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+};
