2a97b763da1ecac96bb51636226e51d247d28ee6
Black Shadow <mithabozdar2@gmail.com>
2025-01-11 16:19:25 +0500

    Add CSS support to WebPack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -20,4 +20,12 @@ export default {
             template: "./src/template.html",
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 };
