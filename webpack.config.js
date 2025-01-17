d6a41c408e179f8f17a6e1506e695b41ade3631f
Black Shadow <mithabozdar2@gmail.com>
2025-01-17 18:39:59 +0500

    Change the state of the Project to Production from Development

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-    mode: "development",
+    mode: "production",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
